457b473c989d16eac08f935e564eed36a8f12011
Squirral Chips <94378581+Kuromishi@users.noreply.github.com>
2023-01-11 13:56:31 +0000

    shooting basic

--- Assets/Scripts/CharacterComponent.cs
+++ Assets/Scripts/CharacterComponent.cs
@@ -10,15 +10,26 @@ public class CharacterComponent : MonoBehaviour
     public float moveSpeed;
     public float jumpHeight;
     public bool canMove;
+    public bool isRotating;
+    public bool canJump;
 
     public float rayLength;
     public float xOffset;
+
+    public float rotateSpeed;
+
+    private Vector3 mousePosition;
+    private Vector2 shootingDirection;
+    public float shootingForce;
+    [SerializeField] private bool isMouseDown;
+    [SerializeField] private bool backToGround;
 
     private Rigidbody2D rb;
 
     private Animator anim;
     private SpriteRenderer sr;
 
+    [Header("State")]
     public bool isPlayerDead;
 
     private void Awake()
@@ -41,16 +52,48 @@ public class CharacterComponent : MonoBehaviour
         if (IsGrounded(xOffset) || IsGrounded(-xOffset)) //two ray, left and right of the body
         {
             anim.SetBool("OnGround", true);
-            if (Input.GetKeyDown(KeyCode.Space) && canMove)
+            backToGround = true;
+
+            if (Input.GetKeyDown(KeyCode.Space) && canMove && isRotating==false)
             {
                 rb.velocity = new Vector2(rb.velocity.x, jumpHeight); //only change y axis
 
             }
+
         }
         else
+        {
             anim.SetBool("OnGround", false);
+        }
 
-        if(isPlayerDead)
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+
+        if (Input.GetKey(KeyCode.S) && canMove)
+        {
+            isRotating = true;
+            anim.SetBool("CanRoll", true);
+        }
+        else if(Input.GetKeyDown(KeyCode.S) && canMove)
+        {
+            isRotating = true;
+            anim.SetBool("CanRoll", true);
+        }
+        else if(Input.GetKeyUp(KeyCode.S) && canMove)
+        {
+            isRotating = false;
+            anim.SetBool("CanRoll", false);
+        }
+
+        if(Input.GetMouseButtonDown(0)&& backToGround) //press LMB and you're on ground
+        {
+            isMouseDown = true;
+
+            //Debug.Log("Shoooooot");
+        }
+
+
+        if (isPlayerDead)
         {
             //Debug.Log("You are dead");
             canMove = false;
@@ -61,6 +104,8 @@ public class CharacterComponent : MonoBehaviour
             Invoke("ReloadScene", 3);
 
         }
+
+
 
     }
 
@@ -76,10 +121,23 @@ public class CharacterComponent : MonoBehaviour
             {
                 transform.localScale = new Vector3(horizontalInput * Mathf.Abs(transform.localScale.x), Mathf.Abs(transform.localScale.y), Mathf.Abs(transform.localScale.z));
             }
+
         }
 
         //Move(horizontalInput, 1);
+        if (isRotating)
+        {
+            shootingDirection = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+            //Debug.Log(shootingDirection);
 
+            if (isMouseDown)
+            {
+                rb.AddForce(shootingDirection * shootingForce, ForceMode2D.Impulse);
+                backToGround = false;
+                isMouseDown = false;
+            }
+
+        }
     }
 
     public bool IsGrounded(float offsetX)

0745848314b180d3a8c269dc7a785be994fe0e73
Squirral Chips <94378581+Kuromishi@users.noreply.github.com>
2023-01-12 16:45:36 +0000

    Update Laser Enemy

--- Assets/Scripts/Enemy/LaserEnemy.cs
+++ Assets/Scripts/Enemy/LaserEnemy.cs
@@ -19,6 +19,14 @@ public class LaserEnemy : MonoBehaviour
     public float maxDistance;
     public LayerMask layerMask;
 
+    public CharacterComponent characterComponent;
+
+    [Header("Feedback")]
+    public ParticleSystem laserParticle1;
+    public ParticleSystem laserParticle2;
+    public ParticleSystem laserParticle3;
+    public ParticleSystem laserParticle4;
+
     private void Start()
     {
 
@@ -31,15 +39,15 @@ public class LaserEnemy : MonoBehaviour
 
         if (isShooting)
         {
-            ShootLaser(laserLine1, firePoint1);
-            ShootLaser(laserLine2, firePoint2);
-            ShootLaser(laserLine3, firePoint3);
-            ShootLaser(laserLine4, firePoint4);
+            ShootLaser(laserLine1, firePoint1, laserParticle1);
+            ShootLaser(laserLine2, firePoint2, laserParticle2);
+            ShootLaser(laserLine3, firePoint3, laserParticle3);
+            ShootLaser(laserLine4, firePoint4, laserParticle4);
 
         }
     }
 
-    public void ShootLaser(LineRenderer laserLine, Transform firePoint )
+    public void ShootLaser(LineRenderer laserLine, Transform firePoint, ParticleSystem laserParticle )
     {
         RaycastHit2D hitInfo = Physics2D.Raycast(firePoint.position, firePoint.up, maxDistance,layerMask);
         
@@ -48,11 +56,14 @@ public class LaserEnemy : MonoBehaviour
         laserLine.SetPosition(0, firePoint.position);
         laserLine.SetPosition(1, hitInfo.point);
 
-        
+        laserParticle.transform.position = hitInfo.point;
+        laserParticle.Play();
 
-        if(hitInfo.collider.gameObject.GetComponentInParent<CharacterComponent>()!=null)  //hit the player
+
+        if (hitInfo.collider.gameObject.GetComponentInParent<CharacterComponent>()!=null)  //hit the player
         {
-           // Debug.Log("Player Died!");
+            // Debug.Log("Player Died!");
+            characterComponent.isPlayerDead = true;
         }
     }
 }
--- Assets/Scripts/UI/StartScene.cs
+++ Assets/Scripts/UI/StartScene.cs
@@ -8,5 +8,26 @@ public class StartScene : MonoBehaviour
     public void StartGame()
     {
         SceneManager.LoadScene(1);
+        GameManager.Instance.isLevel1Finished = false;
+        GameManager.Instance.isLevel2Finished = false;
+        GameManager.Instance.isLevel3Finished = false;
+        GameManager.Instance.isLevel2DialogueShown = false;
+        GameManager.Instance.isLevel2DialogueShown = false;
+        GameManager.Instance.isLevel2DialogueShown = false;
+    }
+    public void ContinueGame()
+    {
+        if(GameManager.Instance.isLevel1Finished)
+        {
+            SceneManager.LoadScene(2);
+        }
+        if (GameManager.Instance.isLevel2Finished)
+        {
+            SceneManager.LoadScene(3);
+        }
+        //if (GameManager.Instance.isLevel3Finished)
+        //{
+        //    SceneManager.LoadScene(4);
+        //}
     }
 }

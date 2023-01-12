0cb34cfb80e7650e3b40dc0fa3de86bf099694b0
Squirral Chips <94378581+Kuromishi@users.noreply.github.com>
2023-01-12 12:19:14 +0000

    Update Bat enemy

--- Assets/Scripts/Enemy/BatEnemy.cs
+++ Assets/Scripts/Enemy/BatEnemy.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 public class BatEnemy : MonoBehaviour
 {
@@ -16,12 +15,14 @@ public class BatEnemy : MonoBehaviour
     [Range(0,1)]
     private float T=0;
 
-    [Header("Irregular movement")]
+    [Header("Random movement")]
     public float batMoveSpeed;
     private float minX, minY, maxX, maxY;
     public Transform moveSpot;
     private float waitTime;
     public float startWaitTime;
+
+    private bool canBatMove;
 
     //Enemy random movement reference: https://www.bilibili.com/video/BV1MJ411X7v4/?spm_id_from=333.337.search-card.all.click&vd_source=492886e5fdb8fffe95e1f3e89e81ae89
 
@@ -32,43 +33,52 @@ public class BatEnemy : MonoBehaviour
         batRenderer = GetComponent<SpriteRenderer>();
         batRB.gravityScale = 0;
 
-        moveSpot.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
-        waitTime = startWaitTime;
-
+        //Set the moving range with player as the center
         minX = transform.position.x - 2;
         minY = transform.position.y - 2;
         maxX = transform.position.x + 2;
         maxY = transform.position.y + 2;
 
+        //set the random move point inside the area set above
+        moveSpot.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+        waitTime = startWaitTime;
 
+        canBatMove = true;
     }
 
     private void Update()
     {
-        //move randomly in a specific area
-        transform.position = Vector2.MoveTowards(transform.position, moveSpot.position, batMoveSpeed * Time.deltaTime);
-
-        if(Vector2.Distance(transform.position, moveSpot.position)<0.2f)
+        if(canBatMove)
         {
-            if(waitTime<=0)
+            //move randomly in a specific area
+            transform.position = Vector2.MoveTowards(transform.position, moveSpot.position, batMoveSpeed * Time.deltaTime);
+
+            if (Vector2.Distance(transform.position, moveSpot.position) < 0.2f) //almost get to the movespot
             {
-                moveSpot.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
-                waitTime = startWaitTime;
-                if(transform.position.x - moveSpot.position.x > 0)
+                if (waitTime <= 0)
                 {
-                    transform.rotation = Quaternion.Euler(0, 180, 0);
+                    //when wait several seconds, set new movespot
+                    moveSpot.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+                    waitTime = startWaitTime;
+
+                    //flip the character
+                    if (transform.position.x - moveSpot.position.x > 0)
+                    {
+                        transform.rotation = Quaternion.Euler(0, 180, 0);
+                    }
+                    else
+                    {
+                        transform.rotation = Quaternion.Euler(0, 0, 0);
+                    }
                 }
                 else
                 {
-                    transform.rotation = Quaternion.Euler(0, 0, 0);
+                    waitTime -= Time.deltaTime;
+
                 }
             }
-            else
-            {
-                waitTime -= Time.deltaTime;
-                
-            }
         }
+        
 
         //bat drop to the ground, and gradually disappear
         if (canBatDisappear)
@@ -96,6 +106,8 @@ public class BatEnemy : MonoBehaviour
             batRB.gravityScale = 1;
             //Instantiate(batBlood, gameObject.transform);
             batBlood.SetActive(true);
+
+            canBatMove = false;
 
         }
         if (collision.gameObject.GetComponent<Ground>() != null)

ceddd0ba96958b7670aa5156420500fb58fe61c8
Squirral Chips <94378581+Kuromishi@users.noreply.github.com>
2023-01-12 12:29:34 +0000

    bat

--- Assets/Scripts/Level/BatGroupControl.cs
+++ Assets/Scripts/Level/BatGroupControl.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelBatComponent : MonoBehaviour
+public class BatGroupControl : MonoBehaviour
 {
     public GameObject batEmeny1;
     public GameObject batEmeny2;
